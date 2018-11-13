84c9a7578c4bb46b1f4eabf1da4f1da4fad3ade6
Clemens Wittenbecher <Clemens.Wittenbecher@dife.de>
2018-11-13 14:37:27 +0100

    use library instead

--- data-raw/simulating-DAG.R
+++ data-raw/simulating-DAG.R
@@ -1,17 +1,17 @@
-# A PDAG
-require(dagitty)
-require(glasso)
+# A DAG
+library(dagitty)
+library(glasso)
 
-g <- dagitty('dag{a -> b[beta=.3] b -> c[beta=.2] c -> d[beta=.5] d -> e[beta=.3] e -> f[beta=.6]   
-             b [beta=.2] b -> k [beta=.3] k -> l [beta=.3] l -> m [beta=.5] ; 
-             m -> o [beta=.3] o -> p [beta=.5]; 
-             a -> v [beta=.2] ; 
-             c -> v [beta=.5] ; 
-             d -> v [beta=.3] ; 
-             e -> m [beta=.5] ; 
-             k -> o [beta=.3] ; 
+g <- dagitty('dag{a -> b[beta=.3] b -> c[beta=.2] c -> d[beta=.5] d -> e[beta=.3] e -> f[beta=.6]
+             b [beta=.2] b -> k [beta=.3] k -> l [beta=.3] l -> m [beta=.5] ;
+             m -> o [beta=.3] o -> p [beta=.5];
+             a -> v [beta=.2] ;
+             c -> v [beta=.5] ;
+             d -> v [beta=.3] ;
+             e -> m [beta=.5] ;
+             k -> o [beta=.3] ;
              o -> p [beta=.65] }')
-x <- simulateSEM(g, N=2500) 
+x <- simulateSEM(g, N=2500)
 
 X_DAG<-est.pcor.skel.DAG.adj(dat=x,alpha_val=0.05)
 plot(X_DAG$skel_est)
