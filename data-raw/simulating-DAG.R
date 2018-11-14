6d0f48698be38b8c1d900db5c10e53eee6b09a3d
Luke Johnston <lwjohnst@gmail.com>
2018-11-14 11:01:58 +0100

    tidied up code, added code to save the data

--- data-raw/simulating-DAG.R
+++ data-raw/simulating-DAG.R
@@ -1,17 +1,34 @@
 # A DAG
 library(dagitty)
 library(glasso)
+library(magrittr)
 
-g <- dagitty('dag{a -> b[beta=.3] b -> c[beta=.2] c -> d[beta=.5] d -> e[beta=.3] e -> f[beta=.6]
-             b [beta=.2] b -> k [beta=.3] k -> l [beta=.3] l -> m [beta=.5] ;
-             m -> o [beta=.3] o -> p [beta=.5];
-             a -> v [beta=.2] ;
-             c -> v [beta=.5] ;
-             d -> v [beta=.3] ;
-             e -> m [beta=.5] ;
-             k -> o [beta=.3] ;
-             o -> p [beta=.65] }')
-x <- simulateSEM(g, N=2500)
+graph <-
+    dagitty("dag{
+        a -> b[beta=.3]
+        b -> c[beta=.2]
+        c -> d[beta=.5]
+        d -> e[beta=.3]
+        e -> f[beta=.6]
+        b [beta=.2]
+        b -> k [beta=.3]
+        k -> l [beta=.3]
+        l -> m [beta=.5]
+        m -> o [beta=.3]
+        o -> p [beta=.5]
+        a -> v [beta=.2]
+        c -> v [beta=.5]
+        d -> v [beta=.3]
+        e -> m [beta=.5]
+        k -> o [beta=.3]
+        o -> p [beta=.65]
+        }")
+
+simulated_data <- simulateSEM(graph, N = 2500) %>%
+    setNames(paste0("metabolite_", 1:length(.)))
+
+devtools::use_data(simulated_data, overwrite = TRUE)
+
 
 X_DAG<-est.pcor.skel.DAG.adj(dat=x,alpha_val=0.05)
 plot(X_DAG$skel_est)
