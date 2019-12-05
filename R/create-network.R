867cb4d80c684ec39741ba1ed18eb281500898dd
Luke Johnston <lwjohnst@gmail.com>
2019-12-05 22:57:15 +0100

    tidied up create network so it only creates network

--- R/create-network.R
+++ R/create-network.R
@@ -1,8 +1,10 @@
 
 #' Create an estimate of the metabolic network skeleton.
 #'
-#' Main NetCoupler network creator. Estimates the skeleton based on a family of
-#' DAGs without specifying the direction of edges.
+#' Estimates the skeleton based on a family of DAGs without specifying the direction of edges.
+#'
+#' Main NetCoupler network creator. Defaults to using the PC algorithm to calculate
+#' possible edges.
 #'
 #' @param .data Data of the metabolic variables.
 #' @param .alpha The alpha level to set.
@@ -11,27 +13,42 @@
 #' @export
 #'
 #' @examples
+#'
 #' library(dplyr)
 #' simulated_data %>%
-#' select(contains("metabolite")) %>%
-#' nc_create_network()
-nc_create_network <- function(.data, .alpha) {
+#'   select(contains("metabolite")) %>%
+#'   nc_create_network()
+nc_create_network <- function(.data, .alpha = 0.05) {
 
-    pcor_matrix <- partial_corr_matrix(.data)
-    skel_est <- pc_skeleton_estimates(.data, .alpha)
-    DAG_est <- pc_dag_estimates(.data, .alpha)
-    adj_matrix <- adjacency_matrix(skel_est)
+    # TODO: Determine if this is important.
+    # pcor_matrix <- partial_corr_matrix(.data)
+
+    # skel_est <- pc_skeleton_estimates(.data, .alpha)
+
+    # TODO: Determine if this is important.
+    # DAG_est <- pc_dag_estimates(.data, .alpha)
+
+    # TODO: Determine if this is important.
+    # adj_matrix <- adjacency_matrix(skel_est)
+
     # TODO: What does the w stand for?
-    w_adj_matrix <- adj_matrix * pcor_matrix
+    # TODO: Determine if this is important.
+    # w_adj_matrix <- adj_matrix * pcor_matrix
 
     # TODO: Rename these to be more descriptive
-    network_list <- list(
-        pCor_mat = pcor_matrix,
-        skel_est = skel_est,
-        DAG_est = DAG_est,
-        adj_matrix = adj_matrix,
-        w_adj_matrix = w_adj_matrix,
-        data = .data
-    )
-    return(network_list)
+    # network_list <- list(
+        # TODO: Is this necessary to output?
+        # pCor_mat = pcor_matrix,
+        # skel_est = skel_est
+        # TODO: Is this necessary to output?
+        # DAG_est = DAG_est,
+        # TODO: Is this necessary to output?
+        # adj_matrix = adj_matrix,
+        # TODO: Is this necessary to output?
+        # w_adj_matrix = w_adj_matrix,
+        # TODO: Is this necessary to output?
+        # data = .data
+    # )
+    # return(network_list)
+    pc_skeleton_estimates(.data, .alpha)
 }
