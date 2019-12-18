fcc06f6ad9e405055aebd13624b8c2d03b340e8f
Luke Johnston <lwjohnst@gmail.com>
2019-12-18 13:03:08 -0500

    start of standardize function

---
+++ R/standardize.R
@@ -0,0 +1,28 @@
+
+# Metabolic variables should be log-transformed and standardized on potential
+# confounders {{this the best way?}} by regressing on age, sex, BMI, and prevalence
+# of hypertension and using the residual variance in further models. Metabolites were
+# then scaled (mean of zero and standard deviation of one). {{both regressed and
+# scaled?}}
+
+nc_standardize <- function(.tbl, .regressed_on = NULL) {
+    if (!is.null(.regressed_on)) {
+        assertive.types::assert_is_character(.regressed_on)
+        .tbl %>%
+            dplyr::mutate_all(.log_regress_standardize, .regressed_on)
+    } else {
+        .tbl %>%
+            dplyr::mutate_all(.log_standardize)
+    }
+}
+
+.log_standardize <- function(x) {
+    log(scale(x))
+}
+
+.log_regress_standardize <- function(x, regressed_on) {
+    # TODO: Decide which method to regress by. lm only?
+    logged_x <- log(x)
+    residual_x <- stats::residuals(stats::glm.fit(logged_x = regressed_on, y = x))
+    scale(residual_x)
+}
