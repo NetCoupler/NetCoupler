1bac89da913a082c621e019d7ad6f81774628838
Luke Johnston <lwjohnst@gmail.com>
2020-01-07 18:55:12 +0100

    create standardize function. Closes #53

--- R/standardize.R
+++ R/standardize.R
@@ -1,28 +1,53 @@
 
-# Metabolic variables should be log-transformed and standardized on potential
-# confounders {{this the best way?}} by regressing on age, sex, BMI, and prevalence
-# of hypertension and using the residual variance in further models. Metabolites were
-# then scaled (mean of zero and standard deviation of one). {{both regressed and
-# scaled?}}
-
-nc_standardize <- function(.tbl, .regressed_on = NULL) {
+#' Standardize the metabolic variables.
+#'
+#' Can standardize by either 1) [log()]-transforming and then applying [scale()]
+#' (mean-center and scaled by standard deviation), or 2) if `.regressed_on`
+#' variables are given, then log-transforming, running a linear regression to obtain
+#' the [stats::residuals()], and finally scaled. Use `.regressed_on` to try to
+#' remove influence of potential confounding.
+#'
+#' @param .tbl Data frame.
+#' @param .vars Metabolic variables that will make up the network.
+#' @param .regressed_on Optional. A character vector of variables to regress the
+#'   metabolic variables on. Use if you want to standardize the metabolic variables
+#'   on variables that are known to influence them, e.g. sex or age. Calculates
+#'   the residuals from a linear regression model.
+#'
+#' @return Outputs a [tibble][tibble::tibble-package] object, with the original metabolic
+#'   variables now standardized.
+#' @export
+#'
+#' @examples
+#'
+#' # Don't regress on any variable.
+#' simulated_data %>%
+#'   nc_standardize(vars(matches("metabolite_"))) %>%
+#'   tibble::as_tibble()
+#'
+#' # Don't regress on any variable.
+#' simulated_data %>%
+#'   nc_standardize(vars(matches("metabolite_")), "age") %>%
+#'   tibble::as_tibble()
+nc_standardize <- function(.tbl, .vars, .regressed_on = NULL) {
     if (!is.null(.regressed_on)) {
         assertive.types::assert_is_character(.regressed_on)
         .tbl %>%
-            dplyr::mutate_all(.log_regress_standardize, .regressed_on)
+            dplyr::mutate_at(.vars, .funs = .log_regress_standardize,
+                             regressed_on = .tbl[.regressed_on])
     } else {
         .tbl %>%
-            dplyr::mutate_all(.log_standardize)
+            dplyr::mutate_at(.vars, .funs = .log_standardize)
     }
 }
 
 .log_standardize <- function(x) {
-    scale(log(x))
+    as.numeric(scale(log(x)))
 }
 
 .log_regress_standardize <- function(x, regressed_on) {
     # TODO: Decide which method to regress by. lm only?
     logged_x <- log(x)
     residual_x <- stats::residuals(stats::glm.fit(y = logged_x, x = regressed_on))
-    scale(residual_x)
+    as.numeric(scale(residual_x))
 }
