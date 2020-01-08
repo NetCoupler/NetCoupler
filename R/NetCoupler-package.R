455e5fd5f3d224c0ba74b940261f52cb23105f83
Luke Johnston <lwjohnst@gmail.com>
2020-01-08 13:10:25 +0100

    add tidyselect functions so matches, vars, etc can be used. Related to #62

--- R/NetCoupler-package.R
+++ R/NetCoupler-package.R
@@ -1,7 +1,7 @@
 #' @import stringr
 #' @importFrom assertive.types assert_is_data.frame assert_is_s4
 #'   assert_is_a_string assert_is_character assert_is_function assert_is_a_number
-#' @importFrom dplyr select mutate everything matches vars select_at
+#' @importFrom dplyr select mutate select_at
 #' @importFrom purrr map map_dfr map2 imap imap_dfr
 #' @keywords internal
 "_PACKAGE"
