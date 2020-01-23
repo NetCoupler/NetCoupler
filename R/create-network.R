bea8faea82533265da8f9d6ca4c88e0e71529e4e
Luke Johnston <lwjohnst@gmail.com>
2020-01-23 11:52:41 +0100

    Start creating function to make network plot
    Related to #80 and #71

--- R/create-network.R
+++ R/create-network.R
@@ -56,3 +56,88 @@ nc_create_network <- function(.data, .alpha = 0.05) {
     # return(network_list)
     pc_skeleton_estimates(.data, .alpha)
 }
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' nc_partial_corr_matrix(.data %>% select(starts_with("mtb_")))
+nc_adjacency_graph <- function(.data, .graph) {
+    weighted_adjacency_matrix <- nc_adjacency_matrix(.graph) *
+        nc_partial_corr_matrix(.data)
+
+    igraph::graph.adjacency(weighted_adjacency_matrix,
+                            weighted = TRUE, mode = "undirected")
+}
+
+nc_plot_network <- function() {
+    adj_graph %>%
+        as_tbl_graph() %>%
+        mutate(name = tidy_metabolic_names(name)) %>%
+        activate(edges) %>%
+        mutate(weight_label = if_else(abs(weight) < 0.4, "", as.character(round(weight, 2)))) %>%
+        ggraph("stress") +
+        geom_edge_bend(aes(
+            colour = weight,
+            width = abs(weight),
+            label = weight_label
+        ),
+        label_dodge =) +
+        scale_edge_colour_gradient2(mid = "gray80") +
+        scale_edge_width(guide = FALSE, range = c(0.75, 2)) +
+        geom_node_text(aes(label = name), repel = TRUE) +
+        ggraph::theme_graph() +
+        coord_cartesian(xlim = c(-2.1, 2.1))
+}
+
+#' Extract adjacency matrix from a DAG skeleton.
+#'
+#' Is generally a wrapper around calls to [igraph::get.adjacency()] and
+#' [igraph::igraph.from.graphNEL()]. Transforms from a GraphNEL object in igraph.
+#'
+#' @param .dag_skeleton The PC DAG skeleton object.
+#'
+#' @return Outputs an adjacency matrix of the DAG skeleton.
+#' @export
+#'
+#' @examples
+#'
+#' library(dplyr)
+#' skeleton_estimate <- simulated_data %>%
+#'   select(contains("metabolite")) %>%
+#'   nc_create_network()
+#'
+#' nc_adjacency_matrix(skeleton_estimate)
+#'
+nc_adjacency_matrix <- function(.dag_skeleton) {
+    # TODO: Include a check here that it is a DAG skeleton..?
+    igraph::get.adjacency(igraph::igraph.from.graphNEL(.dag_skeleton@graph))
+}
+
+#' Estimate Pearson's partial correlation coefficients.
+#'
+#' This function is a wrapper around [ppcor::pcor()] that extracts correlation
+#' coefficient estimates, then adds the variable names to the column and row names.
+#'
+#' @param .data Input data of metabolic variables as matrix or data.frame.
+#'
+#' @return Outputs a matrix of partial correlation coefficients.
+#' @export
+#'
+#' @examples
+#'
+#' library(dplyr)
+#' simulated_data %>%
+#'   select(contains("metabolite")) %>%
+#'   nc_partial_corr_matrix()
+#'
+nc_partial_corr_matrix <- function(.data) {
+    # TODO: check if input data is gaussian
+    pcor_matrix <- ppcor::pcor(.data)$estimate
+    colnames(pcor_matrix) <- colnames(.data)
+    rownames(pcor_matrix) <- colnames(.data)
+    return(pcor_matrix)
+}
