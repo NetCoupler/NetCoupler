5526e6c35a1e98f41adcc7d6dc45cfd9e5be0594
Luke Johnston <lwjohnst@gmail.com>
2020-01-23 11:37:37 +0100

    Trying to get model summary stats
    But right now the statistics are different for different models... and nobs doesn't give same answer...
    
    Related to #69

--- R/clean-models.R
+++ R/clean-models.R
@@ -6,9 +6,17 @@
 }
 
 .tidy_model_output <- function(.object, .exponentiate) {
-    .object %>%
+    model_id <- ids::random_id(1, bytes = 8)
+    model_estimates <- .object %>%
         broom::tidy(exponentiate = .exponentiate, conf.int = TRUE) %>%
         # Give a unique id for the model.
-        mutate(model_id = ids::random_id(1, bytes = 8)) %>%
+        mutate(model_id = model_id)
+
+    model_summary <- .object %>%
+        broom::glance() %>%
+        mutate(model_id = model_id,
+               sample_size = stats::nobs(.object))
+
+    dplyr::left_join(model_estimates, model_summary, by = "model_id") %>%
         select_at(vars("model_id", everything()))
 }
