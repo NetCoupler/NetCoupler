3299628ec4d1e3fd90bab738e50226a8b660effc
Luke Johnston <lwjohnst@gmail.com>
2020-03-13 14:41:51 +0100

    need to import this for ggraph plot to work

--- R/reexports-ggraph.R
+++ R/reexports-ggraph.R
@@ -5,3 +5,7 @@ ggraph::scale_edge_width
 #' @importFrom ggraph scale_edge_colour_gradient
 #' @export
 ggraph::scale_edge_colour_gradient
+
+#' @importFrom ggraph guide_edge_colourbar
+#' @export
+ggraph::guide_edge_colourbar
