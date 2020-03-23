6d84758753d93522ccc80906099bce995be0b7d5
Luke Johnston <lwjohnst@gmail.com>
2020-03-23 19:32:10 +0100

    finished up draft of classifying direct effects
    Resolves #82. Related to #64

--- R/summarize-estimates.R
+++ R/summarize-estimates.R
@@ -1,138 +1,158 @@
-nc_summarize_estimates <- function() {
+#' Classify direct, ambiguous, or no effect between exposure and network nodes.
+#'
+#' @description
+#' \lifecycle{experimental}
+#'
+#' Using the output of the [nc_exposure_estimates()] or [nc_outcome_estimates()],
+#' classify whether the exposure variable has a direct, ambiguous, or no link
+#' with the index node metabolic variable, conditional on neighbouring metablic
+#' variables and on potential confounders.
+#'
+#' @param .tbl Multi-model estimates generated from [nc_exposure_estimates()] or
+#'   [nc_outcome_estimates()] that contain the model summaries.
+#'
+#' @return Outputs a [tibble][tibble::tibble-package] with model estimates
+#'   between the exposure and the individual index network nodes, along with the
+#'   classification of direct effect or not.
+#' @export
+#' @seealso See the `vignette("description")` for a
+#'   detailed description of the algorithm used to classify direct effects.
+#'
+#' @examples
+#'
+#' library(dplyr)
+#' metabolite_network <- simulated_data %>%
+#'   select(matches("metabolite")) %>%
+#'   nc_create_network()
+#' multimodel_exposure <- simulated_data %>%
+#'   mutate(Random = rnorm(nrow(.))) %>%
+#'   nc_exposure_estimates(
+#'     .graph = metabolite_network,
+#'     .exposure = "exposure",
+#'     .adjustment_vars = c("age", "Random"),
+#'     .model_function = lm
+#'    )
+#'
+#' nc_filter_estimates(multimodel_exposure)
+#' nc_classify_effects(multimodel_exposure)
+#'
+#' library(survival)
+#' multimodel_outcome <- simulated_data %>%
+#'   mutate(Random = rnorm(nrow(.))) %>%
+#'   nc_outcome_estimates(
+#'     .graph = metabolite_network,
+#'     .outcome = "survival::Surv(survival_time, case_status)",
+#'     .adjustment_vars = c("age", "Random"),
+#'     .model_function = survival::coxph,
+#'     .exponentiate = TRUE
+#'    )
+#'
+#' nc_filter_estimates(multimodel_outcome)
+#' nc_classify_effects(multimodel_outcome)
+#'
+nc_classify_effects <- function(.tbl) {
+    # TODO: Use an attribute as a "check"
+    assert_is_data.frame(.tbl)
 
+    if (!all(c("index_node", "estimate", "std_error") %in% names(.tbl))) {
+        rlang::abort(c("The data frame provided does not contain the proper columns. Make sure to use the data frame generated from:",
+                       "nc_exposure_estimates()",
+                       "nc_outcome_estimates()"))
+    }
+
+    # Need to round since some p-values can be really small (basically zero),
+    # and others can be exactly zero. So need to assume both are same.
+    rounded_p_values <- .tbl %>%
+        mutate(p_value = round(p_value, 6))
+
+    filtered_estimates <- rounded_p_values %>%
+        nc_filter_estimates()
+
+    external_variable <- filtered_estimates %>%
+        select(matches("^(exposure|outcome)$")) %>%
+        names()
+
+    no_neighbours <- filtered_estimates %>%
+        dplyr::filter(neighbour_vars == "") %>%
+        mutate(no_neighbours_adj_p_value = stats::p.adjust(p_value, "fdr")) %>%
+        dplyr::rename(no_neighbours_p_value = p_value,
+                      no_neighbours_estimate = estimate) %>%
+        select(
+            contains(external_variable),
+            index_node,
+            no_neighbours_adj_p_value,
+            no_neighbours_p_value,
+            no_neighbours_estimate
+        )
+
+    classify_direct_effects <- filtered_estimates %>%
+        mutate(adj_p_value = stats::p.adjust(p_value, "fdr")) %>%
+        dplyr::full_join(no_neighbours, by = c(external_variable, "index_node")) %>%
+        mutate(
+            smaller_pvalue = adj_p_value <= no_neighbours_adj_p_value,
+            same_direction = sign(estimate) == sign(no_neighbours_estimate),
+            smaller_pvalue = dplyr::if_else(neighbour_vars == "", NA, smaller_pvalue),
+            same_direction = dplyr::if_else(neighbour_vars == "", NA, same_direction)
+        ) %>%
+        group_by_at(c(external_variable, "index_node")) %>%
+        mutate(direct_effect = dplyr::case_when(
+            all(smaller_pvalue, na.rm = TRUE) &
+                all(same_direction, na.rm = TRUE) &
+                # When standard error is smaller than the estimate.
+                all(std_error < abs(estimate), na.rm = TRUE) ~ "direct",
+            any(smaller_pvalue, na.rm = TRUE) &
+                any(same_direction, na.rm = TRUE) ~ "ambiguous",
+            TRUE ~ "none"
+        )) %>%
+        dplyr::filter(neighbour_vars == "") %>%
+        select(
+            -statistic, -adjusted_vars,
+            -neighbour_vars, -dplyr::matches("no_neighbour"),
+            -smaller_pvalue, -same_direction
+        )
+
+    return(classify_direct_effects)
 }
 
-# Summary statistics and multiple testing adjustment for net.coupler.out with survival object
+#' @describeIn nc_classify_effects Filter out estimates so only exposure
+#'   estimates are kept. Adds a column with the neighbouring metabolite
+#'   variables listed.
+#' @export
+nc_filter_estimates <- function(.tbl) {
+    .filter_by <- .tbl %>%
+        select(matches("^(exposure|outcome)$")) %>%
+        names()
+    if (.filter_by == "outcome") {
+        .tbl %>%
+            group_by(model_id) %>%
+            mutate(neighbour_vars = .extract_neighbour_nodes(term, adjusted_vars, index_node)) %>%
+            dplyr::ungroup() %>%
+            dplyr::filter(index_node == term) %>%
+            select_at(vars(-term, -model_id))
+    } else {
+        .tbl %>%
+            group_by(model_id) %>%
+            mutate(neighbour_vars = .extract_neighbour_nodes(term, adjusted_vars)) %>%
+            dplyr::ungroup() %>%
+            dplyr::filter(term == .filter_by) %>%
+            select_at(vars(-term, -model_id))
 
-.mult.stat.surv <-
-  function(sum_netout,
-           adjust_method,
-           rule_1 = 12,
-           rule_1_cut = 0.1,
-           rule_2 = 5,
-           rule_2_cut = 0.05,
-           rule_3 = 15,
-           rule_4 = 14,
-           ass_rule1 = 16,
-           round_number) {
+    }
+}
 
-  # sum_netout: output of getExp.coef.out after adding original metabolite names
-  # adjust_method: select adjustment method(s), e.g. "fdr" or c("fdr","bonferroni"), see p.adjust.methods for available options
-  # rule= "set DE=1 or =2 if ..."
-  # rule_1: determines first argument of rule (e.g. adjusted p-value of full model < rule_1_cut)
-  # rule_2: determines second argument of rule (e.g. upperP < rule_2_cut)
-  # rule_3: determines first variable of third argument of rule (e.g. highEst)
-  # rule_4: determines second variable of third argument of rule (e.g. lowEst)
-  # possible arguments for rule_...: adjusted p-values of full model=12=rule_1
-  #                                 largest p-value across all adjacency models=upperP=5=rule_2
-  #                                 highest estimated beta across all adjacency models=highEst=15=rule_3
-  #                                 lowest estimated beta across all adjacency models=lowEst=14=rule_4
-  # default rule: if(adjusted p< 0.1 & upperP<0.05 & (sign(highEst)==sign(lowEst)) & lowEst>0){DE=1}
-  #              if(adjusted p< 0.1 & upperP<0.05 & (sign(highEst)==sign(lowEst)) & lowEst<0){DE=2}else{DE=0}
-  # ass_rule1: determines first variable of second argument of association rule (e.g. bestGuess=16)
-  # default association rule: if(adjusted p<0.1 & bestGuess>0){Assoc=1}
-  #                          if(adjusted p<0.1 & bestGuess<0){Assoc=2}else{Assoc=0}
-  # round_number: actual round number in ambiguous metabolites loop
+.extract_neighbour_nodes <- function(.term_var, .adj_var, .index_node = NULL) {
+    adjusted_variables <- .adj_var %>%
+        unique() %>%
+        strsplit(", ") %>%
+        unlist()
 
-  # return: for each metabolite: maximum number of models, mean hazard ratio, maximum HR, minimum HR, maximum p-value, minimum p-value, as well as number of covariates, HR and p-value for model with full adjustment:
-  sum_netout_sum <-
-    dplyr::left_join(
-      sum_netout %>%
-        dplyr::group_by(Exposure) %>%
-        dplyr::summarise(
-          avgHR = mean(HR),
-          minHR = min(HR),
-          maxHR = max(HR),
-          upperP = max(P),
-          lowerP = min(P),
-          Nbmds = max(Nbmds)
-        ),
-      sum_netout %>%
-        dplyr::group_by(Exposure) %>%
-        dplyr::filter(nchar(Covariables) == max(nchar(Covariables))) %>%
-        dplyr::select(
-          NCov = NCov,
-          Exposure = Exposure,
-          ind_HR = HR,
-          ind_P = P
-        ),
-      by = "Exposure"
-    )
+    if (!is.null(.index_node)) {
+        .index_node <- unique(.index_node)
+        .term_var <- .term_var[!.term_var %in% .index_node]
+    }
 
-  # multiple testing correction:
-  p_adjust_M <- stats::p.adjust.methods[stats::p.adjust.methods %in% adjust_method] # select multiple testing correction method(s)
-  p_adj <- sapply(p_adjust_M, function(meth) {
-    stats::p.adjust(sum_netout_sum$ind_P, meth)
-  }) # calculate adjusted p-values for ind_P
+    neighbour_nodes <- .term_var[!.term_var %in% c("exposure", "outcome", "(Intercept)")]
 
-  sum_netout_sum_adj <- dplyr::bind_cols(sum_netout_sum, data.frame(p_adj))
-  colnames(sum_netout_sum_adj)[12] <- adjust_method
-
-  # add beta-coefficients:
-  sum_netout_sum_adj <-
-    sum_netout_sum_adj %>% dplyr::mutate(
-      avgEst = log(avgHR),
-      lowEst = log(minHR),
-      highEst = log(maxHR),
-      bestGuess = log(ind_HR)
-    )
-
-  # is there an effect of specified metabolite on time-to-event?: determine effect-indicator (DE!=0: direct effect of metabolite on time-to-event, DE=0: ambiguous if adjusted ind_p<0.1):
-  if (!requireNamespace("mosaic")) {
-      stop("Please install the mosaic package to use.")
-  }
-
-  sum_netout_sum_adj <- mutate(sum_netout_sum_adj,
-                               DE = mosaic::derivedFactor(
-                                 "1" = (
-                                   sum_netout_sum_adj[, rule_1] < rule_1_cut &
-                                     sum_netout_sum_adj[, rule_2] < rule_2_cut &
-                                     (sign(sum_netout_sum_adj[, rule_3]) == sign(sum_netout_sum_adj[, rule_4])) &
-                                     sum_netout_sum_adj[, rule_4] > 0
-                                 ),
-                                 "2" = (
-                                   sum_netout_sum_adj[, rule_1] < rule_1_cut &
-                                     sum_netout_sum_adj[, rule_2] < rule_2_cut &
-                                     (sign(sum_netout_sum_adj[, rule_3]) == sign(sum_netout_sum_adj[, rule_4])) &
-                                     sum_netout_sum_adj[, rule_4] < 0
-                                 ),
-                                 .method = "first",
-                                 .default = 0
-                               ))
-
-  # is there any effect of specified metabolite on time-to-event?: determine association-indicator (Assoc!=0: there is an effect (no differenciation between direct or indirect), Assoc=0: there is no effect)
-  if (!requireNamespace("mosaic")) {
-      stop("Please install the mosaic package to use.")
-  }
-
-  sum_netout_sum_adj <- mutate(sum_netout_sum_adj,
-    Assoc = mosaic::derivedFactor(
-      "1" = (sum_netout_sum_adj[, rule_1] < rule_1_cut & sum_netout_sum_adj[, ass_rule1] > 0),
-      "2" = (sum_netout_sum_adj[, rule_1] < rule_1_cut & sum_netout_sum_adj[, ass_rule1] < 0),
-      .method = "first", .default = 0
-    )
-  )
-
-  # collect ambiguous effects:
-  amb <- sum_netout_sum_adj %>% dplyr::filter(Assoc != 0 & DE == 0)
-
-  # collect direct effects:
-  direct <- sum_netout_sum_adj %>% dplyr::filter(Assoc != 0 & DE != 0)
-
-  # collect summary statistics including round information
-  sum_netout_sum_adj_FV <-
-    sum_netout_sum_adj %>% dplyr::mutate(
-      round = as.numeric(round_number),
-      Assoc_FV = as.character(Assoc),
-      DE_FV = as.character(DE)
-    )
-
-  return(
-    list(
-      sum_netout_sum_adj = sum_netout_sum_adj,
-      amb = amb,
-      direct = direct,
-      sum_netout_sum_adj_FV = sum_netout_sum_adj_FV
-    )
-  )
+    neighbour_nodes[!neighbour_nodes %in% adjusted_variables] %>%
+        paste(collapse = ", ")
 }
