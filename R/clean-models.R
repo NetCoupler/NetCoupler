0460b06e9e3b5042bb44e9b723be08b7cc015db8
Luke Johnston <lwjohnst@gmail.com>
2020-03-26 17:46:14 +0100

    add model fit statistics to only lm and glm models. Resolves #69

--- R/clean-models.R
+++ R/clean-models.R
@@ -12,11 +12,33 @@
         # Give a unique id for the model.
         mutate(model_id = model_id)
 
-    model_summary <- .object %>%
-        broom::glance() %>%
-        mutate(model_id = model_id,
-               sample_size = stats::nobs(.object))
+    model_estimates <- model_estimates %>%
+        .conditionally_add_model_summary(.object)
 
-    dplyr::left_join(model_estimates, model_summary, by = "model_id") %>%
+    model_estimates %>%
         select_at(vars("model_id", everything()))
+}
+
+.conditionally_add_model_summary <- function(.tbl, .object) {
+    if (!any(class(.object) %in% c("lm", "glm"))) {
+        return(.tbl)
+    }
+
+    if (requireNamespace("broom", quietly = TRUE)) {
+        model_id <- unique(.tbl$model_id)
+
+        model_summary <- .object %>%
+            broom::glance() %>%
+            mutate(model_id = model_id,
+                   sample_size = stats::nobs(.object)) %>%
+            select(any_of(c("model_id", "r_squared", "adj_r_squared",
+                            "df", "logLik", "AIC", "BIC", "sample_size")))
+
+        summary_with_estimates <- .tbl %>%
+            dplyr::left_join(model_summary, by = "model_id")
+        return(summary_with_estimates)
+    } else {
+        rlang::inform("If you'd like model summary statistics like AIC added to the results, please install the broom package.")
+        return(.tbl)
+    }
 }
