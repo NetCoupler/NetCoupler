5044290a680a0ef2eec791ea9ee7d82fd0e68af2
Luke Johnston <lwjohnst@gmail.com>
2020-03-26 17:54:09 +0100

    one_of is deprecated. Replace with any_of

--- R/reexport-tidyselect.R
+++ R/reexport-tidyselect.R
@@ -4,30 +4,39 @@
 #' @importFrom tidyselect contains
 #' @export
 tidyselect::contains
+
 #' @importFrom tidyselect ends_with
 #' @export
 tidyselect::ends_with
+
 #' @importFrom tidyselect everything
 #' @export
 tidyselect::everything
+
 #' @importFrom tidyselect matches
 #' @export
 tidyselect::matches
+
 #' @importFrom tidyselect num_range
 #' @export
 tidyselect::num_range
-#' @importFrom tidyselect one_of
+
+#' @importFrom tidyselect any_of
 #' @export
-tidyselect::one_of
+tidyselect::any_of
+
 #' @importFrom tidyselect starts_with
 #' @export
 tidyselect::starts_with
+
 #' @importFrom tidyselect last_col
 #' @export
 tidyselect::last_col
+
 #' @importFrom dplyr vars
 #' @export
 dplyr::vars
+
 #' @importFrom tidyselect all_of
 #' @export
 tidyselect::all_of
