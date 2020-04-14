f5cd30fde4f9e4dc86d6e1ee72957c2b80a1807e
Luke Johnston <lwjohnst@gmail.com>
2020-04-14 21:54:15 +0200

    rewrote underlying code for algorithm so it is in one place, resolves #101

--- R/model-estimates.R
+++ R/model-estimates.R
@@ -1,18 +1,37 @@
-#' Estimating pathways to an estimated DAG from an exposure.
+#' Generic function to compute model estimates between an external variable and
+#' a network.
+#'
+#' @param .tbl The data.frame or tibble that contains the variables of interest,
+#'   including the variables passed to the network.
+#' @param .graph Output graph object from `nc_create_network()`.
+#' @param .external_var The variable that links to the network variables
+#'   ("external" to the network).
+#' @param .adjustment_vars Optional. Variables to adjust for in the models.
+#' @param .model_function A function for the model to use (e.g. [stats::lm()],
+#'   [stats::glm()], [survival::coxph()]). Can be any model as long as the
+#'   function has the arguments `formula` and `data`.
+#' @param .model_arg_list Optional. A list containing the named arguments that
+#'   will be passed to the model function. A simple example would be
+#'   `list(family = binomial(link = "logit"))` to specify that the `glm` model
+#'   is a logistic model and not a linear one. See the examples for more on the
+#'   usage.
+#' @param .exponentiate Logical. Whether to exponentiate the log estimates, as
+#'   computed with e.g. logistic regression models.
+#' @param .external_side Character vector. Either "exposure" or "outcome", to
+#'   indicate which side the external variable is on relative to the network.
+#'
+#'   - Exposure indicating the implied directionality is from the external
+#'   variable to the network variable.
+#'   - Outcome indicating the implied directionality is from the network variable
+#'   to the external variable.
 #'
 #' @description
 #' \lifecycle{experimental}
 #'
-#' This algorithm estimates direct effects of a predefined exposure on each
-#' network-variable for all causal models that agree with the input-network:
-#' models are adjusted for all possible combinations of direct neighbors
-#' (==variables in the adjacency set) -> Output is a multiset of possible
-#' effects
+#' TODO: Describe more here (fill out vignette first).
 #'
-#' @param .exposure Character. The exposure variable of interest.
-#'
-#' @return Outputs a [tibble][tibble::tibble-package] with all the models computed and their
-#'   estimates.
+#' @return Outputs a [tibble][tibble::tibble-package] that contains the model
+#'   estimates from either the exposure or outcome side of the network.
 #'
 #' @examples
 #'
@@ -23,109 +42,87 @@
 #' simulated_data %>%
 #'   nc_model_estimates(
 #'     .graph = metabolite_network,
-#'     .exposure = "exposure",
+#'     .external_var = "exposure",
+#'     .external_side = "exposure",
 #'     .adjustment_vars = "age",
-#'     .model_function = lm
+#'     .model_function = glm
+#'    )
+#'
+#' simulated_data %>%
+#'   nc_model_estimates(
+#'     .graph = metabolite_network,
+#'     .external_var = "case_status",
+#'     .external_side = "outcome",
+#'     .adjustment_vars = "age",
+#'     .model_function = glm,
+#'     .model_arg_list = list(family = binomial(link = "logit")),
+#'     .exponentiate = TRUE
 #'    )
 #'
 nc_model_estimates <-
     function(.tbl,
              .graph,
-             .exposure,
+             .external_var,
              .adjustment_vars = NA,
              .model_function,
+             .model_arg_list = NULL,
              .exponentiate = FALSE,
-             ...) {
+             .external_side = c("exposure", "outcome")) {
 
+    # TODO: Use tidy eval style input for variables.
     assert_is_data.frame(.tbl)
     assert_is_s4(.graph)
-    assert_is_a_string(.exposure)
+    assert_is_a_string(.external_var)
     # TODO: This check needs to be better constructed
     if (!missing(.adjustment_vars) | !any(is.na(.adjustment_vars)))
         assert_is_character(.adjustment_vars)
+    if (!is.null(.model_arg_list))
+        assert_is_list(.model_arg_list)
+    assert_is_logical(.exponentiate)
     assert_is_function(.model_function)
 
-    edge_list <- .graph@graph@edgeL
-    edge_table <- as_edge_tbl(edge_list)
+    # TODO: Convert this .graph to an edge table from the beginning (as an input)
+    edge_table <- as_edge_tbl(.graph@graph@edgeL)
 
-    # .external_var
+    network_combinations <- .generate_all_network_combinations(edge_table)
 
-    all_neighbour_combinations <- function(.edge_table) {
-        neighbours <- .edge_table$target_node
-        all_combinations <- lapply(
-            seq_along(neighbours),
-            combn,
-            x = neighbours,
-            simplify = FALSE
-        )
-        all_combinations <- unlist(all_combinations, recursive = FALSE)
-        return(all_combinations)
-    }
+    .external_side <- rlang::arg_match(.external_side)
+    formula_list <- .generate_formula_list(
+        .network_tbl = network_combinations,
+        .ext_var = .external_var,
+        .ext_side = .external_side,
+        .adj_vars = .adjustment_vars
+    )
 
-    split(edge_table, edge_table$source_node) %>%
-        map(all_neighbour_combinations) %>%
-        imap_dfr(
-            ~ tibble(index_node = .y, neighbour_list = .x) %>%
-                dplyr::add_row(index_node = .y, neighbour_list = NULL)
-        ) %>%
-        mutate(external = .external_var) %>%
-        rename_at()
-
-    generate_formula_list <- function(.tbl, .external_side = c("exposure", "outcome"),
-                                      .adjustment_vars) {
-        # external/index variable could be x or y
-        .tbl %>%
-            mutate(x_variables = ~ stats::na.omit(c(.)))
-        x_variables <- stats::na.omit(c(neighbours, adjustments, .exposure))
-        formula <- stats::reformulate(x_variables, response = y)
-        return(formula_list)
-    }
-    all_possible_model_formulas <- network_edges %>%
-        imap( ~ stats::reformulate(stats::na.omit(
-            c(.graph@graph@nodes[.x$edges], .adjustment_vars, .exposure)),
-            response = .y
-        ))
-
-    variables_to_keep <- all_possible_model_formulas %>%
-        purrr::map(all.vars) %>%
+    variables_to_keep <- formula_list %>%
+        map(all.vars) %>%
         purrr::flatten_chr() %>%
         unique()
 
-    # TODO: Right now only complete case in full dataset is allowed, need to consider at model-level
-    .tbl <- .tbl %>%
+    model_data <- .tbl %>%
         select_at(variables_to_keep) %>%
         stats::na.omit()
 
-    all_possible_models <- all_possible_model_formulas %>%
-        map( ~ {
-            # TODO: Need to figure out how to pass other options to function
-            .model_function(formula = .x,
-                            data = .tbl,
-                            na.action = "na.fail")
-        }) %>%
-        map(~ suppressMessages(MuMIn::dredge(.x, fixed = stats::na.omit(c(
-                 .adjustment_vars, .exposure
-             )))))
+    model_arg_list <- list(formula = formula_list)
+    other_args <- list(data = model_data)
+    if (!is.null(.model_arg_list))
+        other_args <- c(other_args, .model_arg_list)
 
-    # TODO: Have argument for threshold? For choosing number of models?
-    # TODO: Don't know why but now message outputs about new name... might be a _dfr thing?
-    tidied_models <- suppressMessages(
-        imap_dfr(all_possible_models,
-                 .extract_and_tidy_models,
-                 .exponentiate = .exponentiate)
-    )
+    model_tbl <- model_arg_list %>%
+        purrr::pmap(purrr::lift_dl(.model_function), other_args) %>%
+        purrr::map2_dfr(network_combinations$index_node,
+                 .tidy_models, .exponentiate = .exponentiate)
 
-    tidied_models <- tidied_models %>%
+    tidied_models <- model_tbl %>%
         mutate(
-            exposure = .exposure,
+            external_var = .external_var,
             adjusted_vars = dplyr::if_else(
                 !any(is.na(.adjustment_vars)),
                 paste(.adjustment_vars, collapse = ", "),
                 NA_character_
             )
         ) %>%
-            # select_at(vars("index_node", everything())))
-        dplyr::relocate(c("exposure", "index_node", "model_id")) %>%
+        dplyr::relocate(c("external_var", "index_node", "model_id")) %>%
         dplyr::rename_all(~ gsub("\\.", "_", .))
 
     return(tidied_models)
@@ -145,3 +142,58 @@ as_edge_tbl <- function(.edge_list) {
     )
     return(edge_table)
 }
+
+.all_neighbour_combinations <- function(.edge_table) {
+    neighbours <- .edge_table$target_node
+    all_combinations <- lapply(seq_along(neighbours),
+                               combn,
+                               x = neighbours,
+                               simplify = FALSE)
+    unlist(all_combinations, recursive = FALSE)
+}
+
+.tidy_models <- function(.object, .index_node, .exponentiate) {
+    model_id <- ids::random_id(1, bytes = 8)
+    model_estimates <- .object %>%
+        broom::tidy(exponentiate = .exponentiate, conf.int = TRUE) %>%
+        # Give a unique id for the model.
+        mutate(model_id = model_id,
+               index_node = .index_node)
+
+    model_estimates %>%
+        .conditionally_add_model_summary(.object)
+}
+
+.generate_all_network_combinations <- function(.edge_tbl) {
+    split(.edge_tbl, .edge_tbl$source_node) %>%
+        map(.all_neighbour_combinations) %>%
+        imap_dfr(
+            ~ tibble(index_node = .y, neighbours = .x) %>%
+                dplyr::add_row(index_node = .y, neighbours = NULL)
+        )
+}
+
+.generate_formula_list <-
+    function(.network_tbl, .ext_var, .ext_side, .adj_vars) {
+        xvars_prep <-
+            list(.network_tbl$neighbours,
+                 .adj_vars) %>%
+            purrr::pmap(c)
+
+        external_input <- switch(
+            .ext_side,
+            exposure = list(y = .network_tbl$index_node,
+                            x = .ext_var),
+            outcome = list(x = .network_tbl$index_node,
+                           y = .ext_var)
+        )
+
+        xvar_input <-
+            list(xvars_prep, external_input$x) %>%
+            purrr::pmap(c) %>%
+            map(stats::na.omit)
+
+        map2(xvar_input,
+             external_input$y,
+             stats::reformulate)
+    }
