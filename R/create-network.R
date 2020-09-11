94c4d48931d5a783519ba412ea8736f7aceaf22e
Luke Johnston <lwjohnst@gmail.com>
2020-09-11 15:49:58 +0200

    moved dag skeleton functions into network file

--- R/create-network.R
+++ R/create-network.R
@@ -108,3 +108,76 @@ nc_partial_corr_matrix <- function(.tbl) {
     rownames(pcor_matrix) <- colnames(.tbl)
     return(pcor_matrix)
 }
+
+#' Estimate equivalence class of DAG from the PC algorithm.
+#'
+#' Is mostly a wrapper around [pcalg::pc()]. Estimates an order-independent
+#' skeleton.
+#'
+#' @param .tbl Input data, samples by metabolite matrix or as data.frame.
+#' @param .alpha Significance level threshold applied to each test.
+#'
+#' @return Outputs a `pcAlgo` object.
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(dplyr)
+#' simulated_data %>%
+#' select(contains("metabolite")) %>%
+#' NetCoupler:::pc_dag_estimates()
+#' }
+#'
+pc_dag_estimates <- function(.tbl, .alpha = 0.01) {
+    number_samples <- nrow(.tbl)
+    metabolite_names <- colnames(.tbl)
+
+    pcalg::pc(
+        suffStat = list(C = stats::cor(.tbl), n = number_samples),
+        indepTest = pcalg::gaussCItest,
+        labels = metabolite_names,
+        skel.method = "stable",
+        alpha = .alpha,
+        fixedGaps = NULL,
+        fixedEdges = NULL,
+        verbose = FALSE,
+        maj.rule = FALSE,
+        solve.confl = FALSE
+    )
+}
+
+#' Estimate order-independent PC-stable skeleton of a DAG.
+#'
+#' Uses the PC-algorithm and is mostly a wrapper around [pcalg::skeleton()].
+#'
+#' @param .tbl Input metabolic data.
+#' @param .alpha Significance level threshold applied to each test.
+#'
+#' @return A DAG skeleton object.
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(dplyr)
+#' simulated_data %>%
+#' select(contains("metabolite")) %>%
+#' NetCoupler:::pc_skeleton_estimates()
+#' }
+#'
+pc_skeleton_estimates <- function(.tbl, .alpha = 0.01) {
+    number_samples <- nrow(.tbl)
+    metabolite_names <- colnames(.tbl)
+
+    # TODO: Confirm that this does this.
+    pcalg::skeleton(
+        suffStat = list(C = stats::cor(.tbl, use = "complete.obs"), n = number_samples),
+        # Test conditional independence of Gaussians via Fisher's Z
+        indepTest = pcalg::gaussCItest,
+        labels = metabolite_names,
+        method = "stable",
+        alpha = .alpha,
+        fixedGaps = NULL,
+        fixedEdges = NULL,
+        verbose = FALSE
+    )
+}
